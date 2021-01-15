17e9eee57822cfaa4544444c66b6a54b7e516260
chenyungui <286224043@qq.com>
2021-01-15 18:02:18 +0800

    add toolset.rb

---
+++ Toolset.rb
@@ -0,0 +1,16 @@
+class Delta < Formula
+    desc "chenyungui's toolset"
+    homepage "https://github.com/chenyunguiMilook"
+    url "https://xal.apuscn.com/services/gods/api/v1/download_app_file/Toolset-0.5.0.tar.gz"
+    sha256 "9d05135c64d8d44a449f68d636a0c259c7d5cc2b29d698ad045fcdb9cd84a8a6"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "toolset"
+    end
+  
+    test do
+      system "#{bin}/toolset", "--version"
+    end
+  end
