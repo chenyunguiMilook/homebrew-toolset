4c0a5c0db1095896ef169bba1f776940cc90421a
chenyungui <286224043@qq.com>
2021-01-19 11:27:15 +0800

    update binary

--- Toolset.rb
+++ Toolset.rb
@@ -1,8 +1,8 @@
 class Toolset < Formula
     desc "chenyungui's toolset"
     homepage "https://github.com/chenyunguiMilook"
-    url "https://xal.apuscn.com/services/gods/api/v1/download_app_file/Toolset-0.5.1.tar.gz"
-    sha256 "3e6b66a20b85a777332c7b24b15088273cbf9c5fa78b957a5636f9c1971e1ca0"
+    url "https://github.com/chenyunguiMilook/homebrew-toolset/releases/download/0.6.0/Toolset-0.6.0.tar.gz"
+    sha256 "5e699942d02920ecad3e697dd7e50c768d06e54a546d195f13a77a1fa89583e6"
   
     bottle :unneeded
   
