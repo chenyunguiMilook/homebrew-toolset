40992287887156c1aba72e99b0f8d100315c6e18
chenyungui <286224043@qq.com>
2021-02-25 12:24:43 +0800

    "update toolset version to: 0.6.6"

--- Toolset.rb
+++ Toolset.rb
@@ -1,8 +1,8 @@
 class Toolset < Formula
     desc "chenyungui's toolset"
     homepage "https://github.com/chenyunguiMilook"
-    url "https://github.com/chenyunguiMilook/homebrew-toolset/releases/download/0.6.5/Toolset-0.6.5.tar.gz"
-    sha256 "9d6d8b52a0acbd214fe47cd3d3bccba16baab217b373281f55d4711f1c3bc6f1"
+    url "https://github.com/chenyunguiMilook/homebrew-toolset/releases/download/0.6.6/Toolset-0.6.6.tar.gz"
+    sha256 "89bc7c9d818c9e7908c453b3cb650a269d723c9ff849b09eefafb627dcc55c89"
   
     bottle :unneeded
   
